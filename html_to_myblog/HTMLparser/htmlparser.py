109695853eefd66db33882c12aa7a7de48290db3
a365541453 <32005940+a365541453@users.noreply.github.com>
2017-11-24 17:31:54 +0800

    20171124

--- html_to_myblog/HTMLparser/htmlparser.py
+++ html_to_myblog/HTMLparser/htmlparser.py
@@ -2,26 +2,10 @@
 # -*- coding: utf-8 -*-
 
 from html.parser import HTMLParser
+import re
 from get_html import get_html
-print('========')
-print(type(get_html.html_file_list))
-print(get_html.html_file_list)
+import sqlite3
 
-exit()
-
-htmlfile = open('html/Linux 运维必备的 13 款实用工具,拿好了~.html', 'r', encoding='utf-8')
-str_html = htmlfile.read()  # 将文件以str的类型读取出来
-htmlfile.close()
-
-##################################以上测试需要########################################
-
-str_html_copy = str_html
-# 因为HTMLParser转换出来的路径是tuple类型，
-# tuple类型的值不能修改
-# 所以复制一份出来，用来在后面步骤中进行str的替换操作
-
-place_list = []
-# 用来存放需要更换的路径名称
 
 class my_html(HTMLParser):
     def handle_starttag(self, tag, attrs):
@@ -32,16 +16,51 @@ class my_html(HTMLParser):
                     place_list.append(value)
                     # 把匹配到需要更换图片的路径放进列表里
 
-a = my_html()
-a.feed(str_html)
+for file in get_html.html_file_list:  # 遍历html文件列表
+    htmlfile = open(file,'r',encoding='utf-8')
+    str_html = htmlfile.read()  # 将文件以str的类型读取出来
+    htmlfile.close()
 
-for place in place_list:  # 遍历需要更换的图片地址
-    str_html_copy = str_html_copy.replace(place, '2222222') #把22222替换成实际的路径
+    str_html_copy = str_html
+    # 因为HTMLParser转换出来的路径是tuple类型，
+    # tuple类型的值不能修改
+    # 所以复制一份出来，用来在后面步骤中进行str的替换操作
+    place_list = []
+    # 用来存放需要更换的路径名称
 
-###################################以下测试需要######################################
+    a = my_html()
+    a.feed(str_html)
 
-out_file = open('C:\\Users\\zwh-pc\\Desktop\\1111.html', 'w', encoding='utf-8')
-out_file.write(str_html_copy)
-out_file.close()
+    for place in place_list:  # 遍历需要更换的图片地址
+        new_place = '/static/upload/kindeditor/'+place
+        str_html_copy = str_html_copy.replace(place, new_place)
+
+
+    str_html_copy = re.search(r'<body>(.|\n)*</body>',str_html_copy).group()
+    #寻找body之间的部分
+
+    str_html_copy = str_html_copy.replace(r'<body>','')
+    str_html_copy = str_html_copy.replace(r'</body>', '')
+    #删除body头
+
+    ###################################写入数据文件######################################
+    db_file = 'C:\\Users\\Administrator\\Desktop\\db.sqlite3'
+    #db = sqlite3.connect(db_file)
+    db_tables = 'linux_part_linux_article'
+
+    ###插入数据
+    title = file.split('/')[2].split('.')[0]
+    type = 5
+    time = '2017-11-24'
+    text = '222222222'
+    sql = 'INSERT INTO %s (title,type,time,text) VALUES (%s,%s,%s,%s)'%(db_tables, title, type, time, text)
+    print(sql)
+    exit()
+
+    db.execute(sql)
+    db.commit()
+    db.close()
+
+
 
 
--- html_to_myblog/get_html/get_html.py
+++ html_to_myblog/get_html/get_html.py
@@ -2,14 +2,29 @@
 # -*- coding: utf-8 -*-
 
 import os
-
+import shutil
 
 dir = '../html_folder/'
 
 list = os.listdir(dir)
-html_file_list = []
+
+html_file = []
+html_dir_list = []
 
 for i in range(0,len(list)):
     path = os.path.join(dir,list[i])
+
     if os.path.isfile(path) and str(path).find('html'):
-        html_file_list.append(path)
+        html_file.append(path)
+
+
+    if os.path.isdir(path):
+        html_dir_list.append(path)
+
+
+#for dir in html_dir_list:
+#    new_dir = r'F:\github\django\django\blog\static\upload\kindeditor'
+#    shutil.move(dir,new_dir)
+
+
+html_file_list = html_file
